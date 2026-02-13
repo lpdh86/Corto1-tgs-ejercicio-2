243db9c0a1654a5668dbd491a6f0d64acfcbd392
lpdh86 <LPDELEONH@correo.url.edu.gt>
2026-02-13 08:17:36 -0600

    Add files via upload

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random r = new Random();
+
+        int pisoActual = 1;
+        int pisoDestino = r.Next(1, 11); 
+
+        Console.WriteLine("El elevador esta en el piso: " + pisoActual);
+        Console.WriteLine("Destino aleatorio: " + pisoDestino);
+        Console.WriteLine();
+
+        while (pisoActual != pisoDestino)
+        {
+            if (pisoActual < pisoDestino)
+            {
+                pisoActual++;
+                Console.WriteLine("Piso: " + pisoActual + "  Direccion: Subiendo");
+            }
+            else
+            {
+                pisoActual--;
+                Console.WriteLine("Piso: " + pisoActual + "  Direccion: Bajando");
+            }
+        }
+
+        Console.WriteLine("\nEl elevador llego al piso destino.");
+    }
+}
